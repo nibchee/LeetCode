a4e3dcb8e4b1d3f97b9f542a417659ee13c649ca
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-02 17:19:20 +0530

    :ballot_box_with_check: search-in-rotated-sorted-array [Runtime: 0 ms; Memory: 41.4 MB]

--- binary-search/search-in-rotated-sorted-array.java
+++ binary-search/search-in-rotated-sorted-array.java
@@ -7,17 +7,17 @@ class Solution {
         int m=l+(h-l)/2;
         if(nums[m]==target)
         return m;
-        else if(nums[l]<=nums[m]){
-            if(target>=nums[l] && target<nums[m])
-            h=m-1;
-            else
+        else if(nums[m]<nums[h]){
+               if(target>=nums[m] && target<=nums[h])
             l=m+1;
+            else
+            h=m-1;
         }
         else{
-            if(target>nums[m] && target<=nums[h])
-            l=m+1;
-            else
+         if(target>=nums[l] && target<=nums[m])
             h=m-1;
+            else
+            l=m+1;
         }
       }   
     return -1;
