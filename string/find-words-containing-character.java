64459c59ca1478a2eba850caf74b7549481010f5
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-03 23:29:05 +0530

    :ballot_box_with_check: find-words-containing-character [Runtime: 1 ms; Memory: 44.6 MB]

---
+++ string/find-words-containing-character.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> indexes=new ArrayList<>();
+       for(int i=0;i<words.length;i++){
+        if(words[i].indexOf(x)>=0)
+        indexes.add(i);
+       }
+    return indexes;
+    }
+}
