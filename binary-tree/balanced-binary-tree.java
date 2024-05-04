656037e373332574b89ca6285ce9787659f96424
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-05-04 13:41:28 +0530

    :ballot_box_with_check: balanced-binary-tree [Runtime: 0 ms; Memory: 44.8 MB]

--- binary-tree/balanced-binary-tree.java
+++ binary-tree/balanced-binary-tree.java
@@ -21,7 +21,7 @@ class Solution {
     public int height(TreeNode root){
         if(root==null) return 0;
         int lh=height(root.left);
-        if(lh==-1) return -1;
+      //  if(lh==-1) return -1;
         int rh=height(root.right);
 
         if(lh==-1 || rh==-1|| Math.abs(lh-rh)>1)
