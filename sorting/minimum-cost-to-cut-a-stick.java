10089dcf391b02953b5f8204225c31ca66d6c987
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-04 14:05:11 +0530

    :ballot_box_with_check: minimum-cost-to-cut-a-stick [Runtime: 10 ms; Memory: 42 MB]

--- sorting/minimum-cost-to-cut-a-stick.java
+++ sorting/minimum-cost-to-cut-a-stick.java
@@ -1,14 +1,17 @@
 class Solution {
     int dp[][];
-    private int helper(int i,int j,int[] cuts){
-        if(j-i==1) return 0;
-        if(dp[i][j]!=0) return dp[i][j];
-    int min_cost=Integer.MAX_VALUE;
-        for(int idx=i+1;idx<=j-1;idx++){
-          int cost=(cuts[j]-cuts[i])+helper(i,idx,cuts)+helper(idx,j,cuts);
-          min_cost=Math.min(min_cost,cost);
+
+    private int helper(int i, int j, int[] cuts) {
+        if (j - i == 1)
+            return 0;
+        if (dp[i][j] != 0)
+            return dp[i][j];
+        int min_cost = Integer.MAX_VALUE;
+        for (int idx = i + 1; idx <= j - 1; idx++) {
+            int cost = (cuts[j] - cuts[i]) + helper(i, idx, cuts) + helper(idx, j, cuts);
+            min_cost = Math.min(min_cost, cost);
         }
-        return dp[i][j]=min_cost;
+        return dp[i][j] = min_cost;
     }
 
     public int minCost(int n, int[] cuts) {
@@ -16,12 +19,24 @@ class Solution {
         int[] newCuts = new int[m + 2];
         for (int i = 0; i < m; i++)
             newCuts[i] = cuts[i];
-        
+
         newCuts[m] = n;
         Arrays.sort(newCuts);
-        dp=new int[m+2][m+2];
-        
-        return helper(0, m + 1, newCuts);
+        dp = new int[m + 2][m + 2];
 
+        for (int i = m+1; i >=0; i--) {
+            for (int j = 0; j <m+2; j++) {
+                if(i>=j || Math.abs(j-i)==1) continue;
+                int min_cost=Integer.MAX_VALUE;
+                for (int idx = i + 1; idx <= j - 1; idx++) {
+                    int cost = (newCuts[j] - newCuts[i]) + dp[i][idx] + dp[idx][j];
+                    min_cost = Math.min(min_cost, cost);
+                }
+                dp[i][j]=min_cost;
+            }
+        }
+
+      //  return helper(0, m + 1, newCuts);
+     return dp[0][m+1];
     }
 }
