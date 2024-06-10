cf04ef016200bdb345095016742d5c7d4349ff5f
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-10 14:11:18 +0530

    :ballot_box_with_check: powx-n [Runtime: 0 ms; Memory: 41.5 MB]

--- math/powx-n.java
+++ math/powx-n.java
@@ -1,37 +1,14 @@
 class Solution {
     public double myPow(double x, int n) {
-        boolean negativeSign = false;
-        boolean isMaxMin=false;
-        if(n==Integer.MIN_VALUE){
-            n+=1;
-            isMaxMin=true;
-        }
-        if (n < 0) {
-            negativeSign = true;
-            n = -n;
-        }
-
-        double ans = helper(x, n);
-      
-      if(isMaxMin){
-             ans=ans*x;    
-            }
-        if (negativeSign)
-            return 1 / ans;
-
-            
-        return ans;
-    }
-
-    private double helper(double x, int n) {
-        if(n==0) return 1;
+        if (n < 0)
+            return 1 / (x * myPow(x, -(n + 1)));
         if (n == 1)
             return x;
 
-        double ans = helper(x, n / 2);
-        ans *= ans;
-        if (n % 2 == 1)
-            ans = ans * x;
-        return ans;
+        if (n == 0)
+            return 1.0;
+
+        double result = myPow(x, n / 2);
+        return n % 2 == 0 ? result * result : result * result * x;
     }
 }
