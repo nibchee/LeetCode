8e12d408ae98ad664be36f2865f15e374e0b019b
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-12 14:21:36 +0530

    :ballot_box_with_check: sort-colors [Runtime: 0 ms; Memory: 41.6 MB]

--- sorting/sort-colors.java
+++ sorting/sort-colors.java
@@ -1,30 +1,30 @@
 class Solution {
-    private void swap(int arr[],int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    private void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
+
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        int l=0,m=0,r=n-1;
-        while(m<=r){
-            switch(nums[m]){
+        int n = nums.length;
+        int l = 0, m = 0, r = n - 1;
+        while (m <= r) {
+            switch (nums[m]) {
                 case 0:
-                   swap(nums,m,l);
-                   l++;
-                   m++;
-                   break;
+                    swap(nums, m, l);
+                    l++;
+                    m++;
+                    break;
                 case 1:
                     m++;
-                  break;
+                    break;
                 case 2:
-                swap(nums,m,r);
-                r--;
-                break;
+                    swap(nums, m, r);
+                    r--;
+                    break;
             }
         }
 
     }
-
 
 }
