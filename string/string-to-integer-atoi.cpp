7cd5282bd33c04b28db35e17cf91644056dfc7eb
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-08-11 21:23:18 +0530

    :ballot_box_with_check: string-to-integer-atoi [Runtime: 2 ms; Memory: 9 MB]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     int myAtoi(string s) {
         int i = 0;
-        int flag = 1; 
-        
-        while (i < s.size() && s[i] == ' ') {
+        int flag = 0;
+        while (s[i] == ' ') {
             i++;
         }
-        
-        if (i < s.size() && s[i] == '-') {
+        if (s[i] == '-') {
             flag = -1;
             i++;
-        } else if (i < s.size() && s[i] == '+') {
+        } else if (s[i] == '+') {
+            flag = 1;
             i++;
         }
-        
         int n = 0;
-        
-        while (i < s.size() && isdigit(s[i])) {
-            int digit = s[i] - '0';
-            
-            if (n > (INT_MAX - digit) / 10) {
-                return flag == -1 ? INT_MIN : INT_MAX;
+        for (int j = i; j < s.size(); j++) {
+            if (isdigit(s[j])) {
+                int digit = s[j] - '0';
+                if (n > ((INT_MAX - digit) / 10) ||
+                    (n == (INT_MAX / 10) && digit > 7)) {
+                    return flag == -1 ? INT_MIN : INT_MAX;
+                }
+                n = n * 10 + digit;
+
+            } else {
+                break;
             }
-            
-            n = n * 10 + digit;
-            i++;
         }
-        
-        return flag * n;
+        return flag == -1 ? flag * n : n;
     }
 };
