7b6654aded82f8b00ead207256f0eb374133f9cd
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-08-11 21:19:19 +0530

    :ballot_box_with_check: string-to-integer-atoi [Runtime: 6 ms; Memory: 9 MB]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int myAtoi(string s) {
         int i = 0;
-        int flag = 1;  // Initialize flag to 1 for positive numbers
+        int flag = 1; 
         
-        // Skip leading whitespace
         while (i < s.size() && s[i] == ' ') {
             i++;
         }
         
-        // Check for sign
         if (i < s.size() && s[i] == '-') {
             flag = -1;
             i++;
@@ -19,11 +17,9 @@ public:
         
         int n = 0;
         
-        // Convert digits to number
         while (i < s.size() && isdigit(s[i])) {
             int digit = s[i] - '0';
             
-            // Check for overflow before multiplying and adding
             if (n > (INT_MAX - digit) / 10) {
                 return flag == -1 ? INT_MIN : INT_MAX;
             }
