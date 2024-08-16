baf00d42b1dc179d1e54e0182e15dec8aa0d54f3
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-08-16 20:30:41 +0530

    LC-maximum-distance-in-arrays [Runtime: 12 ms; Memory: 64.3 MB]

--- array/maximum-distance-in-arrays.java
+++ array/maximum-distance-in-arrays.java
@@ -1,14 +1,14 @@
 public class Solution {
-    public int maxDistance(int[][] arrays) {
+    public int maxDistance(List<List<Integer>> arrays) {
         int result = Integer.MIN_VALUE;
-        int max = arrays[0][arrays[0].length - 1];
-        int min = arrays[0][0];
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        int min = arrays.get(0).get(0);
         
-        for (int i = 1; i < arrays.length; i++) {
-            result = Math.max(result, Math.abs(arrays[i][0] - max));
-            result = Math.max(result, Math.abs(arrays[i][arrays[i].length - 1] - min));
-            max = Math.max(max, arrays[i][arrays[i].length - 1]);
-            min = Math.min(min, arrays[i][0]);
+        for (int i = 1; i < arrays.size(); i++) {
+            result = Math.max(result, Math.abs(arrays.get(i).get(0) - max));
+            result = Math.max(result, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min));
+            max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
+            min = Math.min(min, arrays.get(i).get(0));
         }
         
         return result;
