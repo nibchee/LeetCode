cdaea549fcb9e9169401926b0bc54e84c5e02295
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-09-22 06:30:48 +0530

    LC-k-th-smallest-in-lexicographical-order [Runtime: N/A; Memory: N/A]

--- trie/k-th-smallest-in-lexicographical-order.java
+++ trie/k-th-smallest-in-lexicographical-order.java
@@ -1,20 +1,26 @@
 class Solution {
+  int c=0;
+   Integer ans=-1;
     public int findKthNumber(int n, int k) {
-          List<Integer> ans=new ArrayList<>();
+         
       for(int s=1;s<=9;s++){
-        helper(s,n,ans,k);
+        helper(s,n,k);
+        if(c==k)
+        break;
       } 
-      return ans.get(k-1);
+      return ans;
     }
 
-     private void helper(int num,int limit,List<Integer> ans,int k){
+     private void helper(int num,int limit,int k){
       if(num>limit) return;
-      if(ans.size()>=k) return;
-      ans.add(num);
+      if(c>=k) return;
+      ans=num;
+      c++;
+      
       for(int nextDigit=0;nextDigit<=9;nextDigit++){
         int nextNum=num*10+nextDigit;
         if(nextNum>limit) break;
-        helper(nextNum,limit,ans,k);
+        helper(nextNum,limit,k);
       }
     }
 }
