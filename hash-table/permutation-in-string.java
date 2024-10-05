7a706ad1b882eb0eeddc9a50b5fff785133598e8
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-10-05 23:27:47 +0530

    LC-permutation-in-string [Runtime: 10 ms; Memory: 42.5 MB]

--- hash-table/permutation-in-string.java
+++ hash-table/permutation-in-string.java
@@ -1,30 +1,37 @@
 public class Solution {
-    boolean flag = false;
-    
     public boolean checkInclusion(String s1, String s2) {
-        permute(s1, s2, 0);
-        return flag;
-    }
-    
-    public String swap(String s, int i0, int i1) {
-        if (i0 == i1)
-            return s;
-        String s1 = s.substring(0, i0);
-        String s2 = s.substring(i0 + 1, i1);
-        String s3 = s.substring(i1 + 1);
-        return s1 + s.charAt(i1) + s2 + s.charAt(i0) + s3;
-    }
-    
-    void permute(String s1, String s2, int l) {
-        if (l == s1.length()) {
-            if (s2.indexOf(s1) >= 0)
-                flag = true;
-        } else {
-            for (int i = l; i < s1.length(); i++) {
-                s1 = swap(s1, l, i);
-                permute(s1, s2, l + 1);
-                s1 = swap(s1, l, i);
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1arr[s1.charAt(i) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] == s2arr[i])
+                count++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2arr[r]++;
+            if (s2arr[r] == s1arr[r]) {
+                count++;
+            } else if (s2arr[r] == s1arr[r] + 1) {
+                count--;
+            }
+            s2arr[l]--;
+            if (s2arr[l] == s1arr[l]) {
+                count++;
+            } else if (s2arr[l] == s1arr[l] - 1) {
+                count--;
             }
         }
+        return count == 26;
     }
 }
