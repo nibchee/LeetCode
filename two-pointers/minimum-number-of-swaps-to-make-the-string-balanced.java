d05dbfa5e19950dab4d82e46cf65b2903d5f6c5d
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-10-08 20:45:20 +0530

    LC-minimum-number-of-swaps-to-make-the-string-balanced [Runtime: N/A; Memory: N/A]

--- two-pointers/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ two-pointers/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -6,23 +6,24 @@ class Solution {
         int n=s.length();
         int j=n-1;
         int ans=0;
-        while(i<=j){
-          while(i<=j && start>=0){
+        while(i<j){
+          while(i<j && start>=0){
              if(s.charAt(i)==']')
              start--;
              else
              start++;
              i++;
           } 
-          while(j>=i && end>=0){
+          while(j>i && end>=0){
             if(s.charAt(j)=='[')
              end--;
              else
              end++;
              j--;
           }
+          if(i==n || j==-1) break;
           System.out.println(i+" "+j);
-          if(i!=n && j!=-1 && s.charAt(i-1)==']' && s.charAt(j+1)=='['){
+          if(s.charAt(i-1)==']' && s.charAt(j+1)=='['){
              ans++;
              start++;
              end++;
