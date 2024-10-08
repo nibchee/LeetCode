e3c753e4ed6d460490026adb57b6da0981993d78
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-10-08 21:00:56 +0530

    LC-minimum-number-of-swaps-to-make-the-string-balanced [Runtime: 88 ms; Memory: 55 MB]

--- string/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ string/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,34 +1,19 @@
 class Solution {
     public int minSwaps(String s) {
-        int start=0;
-        int end=0;
-        int i=0;
-        int n=s.length();
-        int j=n-1;
-        int ans=0;
-        while(i<j){
-          while(i<j && start>=0){
-             if(s.charAt(i)==']')
-             start--;
-             else
-             start++;
-             i++;
-          } 
-          while(j>i && end>=0){
-            if(s.charAt(j)=='[')
-             end--;
-             else
-             end++;
-             j--;
-          }
-          if(i==n || j==-1) break;
-          System.out.println(i+" "+j);
-          if(s.charAt(i-1)==']' && s.charAt(j+1)=='['){
-             ans++;
-             start++;
-             end++;
-          }
-        }
-    return ans;
+       Stack<Character> stack=new Stack();
+       int unbalanced=0;
+
+       for(int i=0;i<s.length();i++){
+         char ch=s.charAt(i);
+         if(ch=='[')
+         stack.push(ch);
+         else{
+          if(!stack.isEmpty())
+          stack.pop();
+          else
+          unbalanced++;
+         }
+       }
+       return (unbalanced+1)/2; 
     }
 }
