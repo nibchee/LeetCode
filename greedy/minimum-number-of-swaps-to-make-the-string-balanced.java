041423b6e05e1141f13f317ccad87a9b2b3b56e1
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-10-08 20:31:11 +0530

    LC-minimum-number-of-swaps-to-make-the-string-balanced [Runtime: N/A; Memory: N/A]

--- greedy/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ greedy/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -6,23 +6,23 @@ class Solution {
         int n=s.length();
         int j=n-1;
         int ans=0;
-        while(i<j){
-          while(i<j && start>=0){
+        while(i<=j){
+          while(i<=j && start>=0){
              if(s.charAt(i)==']')
              start--;
              else
              start++;
              i++;
           } 
-          while(j>i && end>=0){
+          while(j>=i && end>=0){
             if(s.charAt(j)=='[')
              end--;
              else
              end++;
              j--;
           }
-
-          if(i<j){
+          System.out.println(i+" "+j);
+          if(s.charAt(i-1)==']' && s.charAt(j+1)=='['){
              ans++;
              start++;
              end++;
