48e2308408a52d73244794d0ed495d1a55ce7155
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-10-16 01:06:23 +0530

    LC-find-x-sum-of-all-k-long-subarrays-ii [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/find-x-sum-of-all-k-long-subarrays-ii.java
+++ heap-priority-queue/find-x-sum-of-all-k-long-subarrays-ii.java
@@ -1,14 +1,53 @@
 class Solution {
-    public long[] findXSum(int[] nums, int k, int x) {
-     HashMap<Integer,Integer> map=new HashMap<>();
-      PriorityQueue<Integer> pq=new PriorityQueue<>(
-        (a,b)->{
-          return map.get(a)-map.get(b)
-        }
-       ); 
+  public long[] findXSum(int[] nums, int k, int x) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int n = nums.length;
+    PriorityQueue<Integer> pq = new PriorityQueue<>(x,
+        (a, b) -> {
+          if (map.get(b) == map.get(a))
+            return b - a;
+          return map.get(b) - map.get(a);
+        });
 
-   return new long[0];
+    for (int i = 0; i < k; i++) {
+      map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
     }
 
-   
+    for (int i = 0; i < k; i++) {
+      if (!pq.contains(nums[i]))
+        pq.add(nums[i]);
+    }
+
+    int j = k - 1, i = 0;
+    long ans[] = new long[n - k + 1];
+
+    while (j < nums.length) {
+      long sum = 0;
+      for (int y = 0; y < x; y++) {
+        int e = pq.remove();
+        //System.out.println("ele " + e + " " + map.get(e));
+        sum += e * map.get(e);
+      }
+      while (!pq.isEmpty()) {
+        pq.remove();
+      }
+      //System.out.println("Sum " + sum);
+      map.put(nums[i], map.get(nums[i]) - 1);
+      if (map.get(nums[i]) == 0)
+        map.remove(nums[i]);
+      ans[i] = sum;
+      i++;
+      j++;
+      if (j < n) {
+        map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+      }
+      for (int z = i; z <= j && z < n; z++) {
+        if (!pq.contains(nums[z]))
+          pq.add(nums[z]);
+      }
+    }
+
+    return ans;
+  }
+
 }
