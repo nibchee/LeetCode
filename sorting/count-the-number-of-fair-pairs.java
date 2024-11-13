4158a6a45353bac1b39b6e2b8c56a43df64f6411
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-11-13 16:19:16 +0530

    LC-count-the-number-of-fair-pairs [Runtime: 65 ms; Memory: 57.3 MB]

--- sorting/count-the-number-of-fair-pairs.java
+++ sorting/count-the-number-of-fair-pairs.java
@@ -1,25 +1,9 @@
 class Solution {
-    public long countFairPairs(int[] nums, int lower, int upper) {
-        Arrays.sort(nums);
-        int n=nums.length;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(lower-nums[i]<=0)
-            break;
-            int smallest=greaterThanEqualTo(nums,lower-nums[i],i);
-            int largest=lessThanEqualTo(nums,upper-nums[i],i);
-            ans+=largest-smallest+1;
-            System.out.println(nums[i]+" "+smallest+" "+largest);
-        }
-        return ans+1;
-    }
-    
-    private int greaterThanEqualTo(int nums[],int target,int lower){
-          int low=lower;
-          int high=nums.length-1;
-         while (low <= high) {
+
+    long lower_bound(int[] nums, int low, int high, int element) {
+        while (low <= high) {
             int mid = low + ((high - low) / 2);
-            if (nums[mid] >= target) {
+            if (nums[mid] >= element) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -27,17 +11,27 @@ class Solution {
         }
         return low;
     }
-   private int lessThanEqualTo(int nums[],int target,int lower){
-int low=lower;
-          int high=nums.length-1;
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (nums[mid] >= target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+
+    long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            long low = lower_bound(
+                nums,
+                i + 1,
+                nums.length - 1,
+                lower - nums[i]
+            );
+
+            long high = lower_bound(
+                nums,
+                i + 1,
+                nums.length - 1,
+                upper - nums[i] + 1
+            );
+
+            ans += 1 * (high - low);
         }
-        return high;
+        return ans;
     }
 }
