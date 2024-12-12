28e3656b569ce8dd4674a633ef18307697c68411
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-12-12 12:30:01 +0530

    LC-number-of-provinces [Runtime: 1 ms; Memory: 48.6 MB]

--- depth-first-search/number-of-provinces.java
+++ depth-first-search/number-of-provinces.java
@@ -1,28 +1,27 @@
 class Solution {
-    boolean [][]visited;
+    boolean []visited;
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-        visited=new boolean[n][n];
+        visited=new boolean[n];
         int ans=0;
-        for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-            if(!visited[i][j] && isConnected[i][j]!=0){
-            dfs(i,j,isConnected);
-           ans++;
+        for(int node=0;node<n;node++){
+          //  System.out.println(node+" "+visited[node]);
+            if(!visited[node]){
+            dfs(node,isConnected);
+            ans++;
             }
-           }
-       
         }
+        
  return ans;
     }
 
-    public void dfs(int i,int j,int[][] graph){
-        if(i<0 || i>=graph.length || j<0 || j>=graph.length || visited[i][j] || graph[i][j]==0)
-        return;
-        visited[i][j]=true;
-        dfs(i+1,j,graph);
-        dfs(i-1,j,graph);
-        dfs(i,j+1,graph);
-        dfs(i,j-1,graph);
+    public void dfs(int node,int[][] graph){
+        visited[node]=true;
+
+        for(int k=0;k<graph[node].length;k++){
+            if(!visited[k] && graph[node][k]==1){
+                dfs(k,graph);
+            }
+        }
     }
 }
