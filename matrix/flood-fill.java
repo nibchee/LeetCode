d6eeb249784f1206f452c37fb6be674ac2283ae2
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-12-12 13:31:00 +0530

    LC-flood-fill [Runtime: 0 ms; Memory: 45 MB]

--- matrix/flood-fill.java
+++ matrix/flood-fill.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-       // int oldcolor=image[sr][sc];
-        dfs(image,sr,sc,color);
+       int oldcolor=image[sr][sc];
+        dfs(image,sr,sc,color,oldcolor);
         return image;
     }
 
-    private void dfs(int[][]img,int i,int j,int color){
-        if(i<0 || i>=img.length || j<0 || j>=img[0].length || img[i][j]==0 || img[i][j]==color)
+    private void dfs(int[][]img,int i,int j,int color,int oldcolor){
+        if(i<0 || i>=img.length || j<0 || j>=img[0].length || img[i][j]==color|| img[i][j]!=oldcolor)
         return;
 
         img[i][j]=color;
-        dfs(img,i-1,j,color);
-        dfs(img,i+1,j,color);
-        dfs(img,i,j-1,color);
-        dfs(img,i,j+1,color);
+        dfs(img,i-1,j,color,oldcolor);
+        dfs(img,i+1,j,color,oldcolor);
+        dfs(img,i,j-1,color,oldcolor);
+        dfs(img,i,j+1,color,oldcolor);
     }
 
 
