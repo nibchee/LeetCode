f4cd058e1f63aba8360866d28ca631f3912b76ed
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-12-15 18:49:09 +0530

    LC-maximum-average-pass-ratio [Runtime: 543 ms; Memory: 75.5 MB]

--- array/maximum-average-pass-ratio.java
+++ array/maximum-average-pass-ratio.java
@@ -2,17 +2,14 @@ import java.util.PriorityQueue;
 
 class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        // PriorityQueue to store classes based on their pass ratio improvement
         PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> 
             Double.compare((double)(b[0] + 1) / (b[1] + 1) - (double)(b[0]) / b[1], 
                            (double)(a[0] + 1) / (a[1] + 1) - (double)(a[0]) / a[1]));
 
-        // Add all classes to the heap
         for (int[] currentClass : classes) {
             heap.add(currentClass);
         }
 
-        // Allocate extra students to improve the pass ratio
         while (extraStudents > 0) {
             int[] highPassRatioClass = heap.remove();
             highPassRatioClass[0] += 1;
@@ -21,7 +18,6 @@ class Solution {
             extraStudents--;
         }
 
-        // Calculate the total average pass ratio
         double ans = 0;
         while (!heap.isEmpty()) {
             int[] currentClass = heap.remove();
