7ecff5977717b03d2fd3661cbd699b16129a9788
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-06 01:26:00 +0530

    LC-count-number-of-nice-subarrays [Runtime: 11 ms; Memory: 55 MB]

--- sliding-window/count-number-of-nice-subarrays.java
+++ sliding-window/count-number-of-nice-subarrays.java
@@ -1,16 +1,22 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        int currSum=0,subarrays=0;
-        Map<Integer,Integer> prefixSum=new HashMap<>();
-        prefixSum.put(currSum,1);
 
-        for(int i=0;i<nums.length;i++){
-            currSum+=nums[i]%2;
-            if(prefixSum.containsKey(currSum-k)){
-                subarrays+=prefixSum.get(currSum-k);
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        int windowSize = 0, subarrays = 0, start = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            windowSize += nums[end] % 2;
+           
+            while (windowSize > k) {
+                windowSize -= nums[start] % 2;
+                start++;
             }
-            prefixSum.put(currSum,prefixSum.getOrDefault(currSum,0)+1);
+            subarrays += end - start + 1;
         }
+
         return subarrays;
     }
 }
