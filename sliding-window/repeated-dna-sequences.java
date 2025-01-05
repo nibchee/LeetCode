fd20ef310045c85e2736464360d72ff99fbcd2e0
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-05 23:51:41 +0530

    LC-repeated-dna-sequences [Runtime: 25 ms; Memory: 53.8 MB]

--- sliding-window/repeated-dna-sequences.java
+++ sliding-window/repeated-dna-sequences.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         HashMap<String,Integer> freq=new HashMap<>();
-        for(int i=0;i<s.length()-10;i++){
+        for(int i=0;i<=s.length()-10;i++){
             String s2=s.substring(i,i+10);
             freq.put(s2,freq.getOrDefault(s2,0)+1);
         }
@@ -14,3 +14,4 @@ class Solution {
         return ans;
     }
 }
+// //0 1 2 3 4 5 6 7 8 9 10
