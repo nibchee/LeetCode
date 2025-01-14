6f3844b6f6e6cae3203d97a1616fdeb35178449a
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-15 02:35:07 +0530

    LC-sum-of-scores-of-built-strings [Runtime: 13 ms; Memory: 45.4 MB]

--- binary-search/sum-of-scores-of-built-strings.java
+++ binary-search/sum-of-scores-of-built-strings.java
@@ -2,15 +2,32 @@ class Solution {
     public long sumScores(String s) {
         int n=s.length();
         int[] z=new int[n];
-        
-        for(int i=0;i<n;i++){
+
+       //O(n^2) 
+        // for(int i=0;i<n;i++){
+        //     while((i+z[i]<n) && (s.charAt(z[i])==s.charAt(i+z[i])))
+        //     z[i]++;
+        // }
+
+        int l=0;
+        int r=0;
+        //O(n)
+        for(int i=1;i<n;i++){
+            if(i<=r){
+                z[i]=Math.min(r-i+1,z[i-l]);
+            }
             while((i+z[i]<n) && (s.charAt(z[i])==s.charAt(i+z[i])))
             z[i]++;
+
+            if(i+z[i]-1>r){
+                l=i;
+                r=i+z[i]-1;
+            }
         }
         long ans=0;
         for(int e:z)
         ans+=e;
 
-        return ans;
+        return ans+n;
     }
 }
