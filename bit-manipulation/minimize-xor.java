dcd3c26b9fee36bd64cdec5387ac63eb3b523705
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-15 23:24:26 +0530

    LC-minimize-xor [Runtime: N/A; Memory: N/A]

--- bit-manipulation/minimize-xor.java
+++ bit-manipulation/minimize-xor.java
@@ -10,29 +10,56 @@ class Solution {
     }
 
     public int minimizeXor(int num1, int num2) {
-        int bits = countBits(num2);
-       // System.out.println(bits);
-        int k = 0;
-        int ans = 0;
-        while (num1 != 0) {
-
-            while (num1!=0 && (num1 & 1) == 0) {
-                num1 = num1 >> 1;
-                k++;
-            }
-           // System.out.println(k);
-            ans += Math.pow(2, k);
-            bits--;
-            num1 = num1 >> 1;
-                k++;
+        int b1=countBits(num2);
+        PriorityQueue<Integer> q=new PriorityQueue<>(b1);
+        int pos=0;
+        while(num1!=0){
+            if((num1 & 1)==1)
+            q.add(pos);
+           
+           if(q.size()>b1) q.remove();
+           num1=num1>>1;
+            pos++;
         }
 
-        while (bits != 0) {
-            //System.out.println(k);
-            ans += Math.pow(2, k);
-            bits--;
-            k++;
+        int ans=0;
+        int ele=0;
+        int qSize=q.size();
+        while(!q.isEmpty()){
+                 ele=(int)q.remove();
+                ans+=Math.pow(2,ele);
         }
-        return ans;
+         ele++;
+        for(int i=0;i<(b1-qSize);i++){
+            ans+=Math.pow(2,ele);
+            ele++;
+        }
+    return ans;
     }
 }
+
+// int bits = countBits(num2);
+//         int bits2=countBits(num1);
+//          System.out.println("Bits"+bits);
+//         int k = 0;
+//         int ans = 0;
+//         while (num1 != 0 && bits!=0) {
+
+//             while (num1 != 0 && (num1 & 1) == 0) {
+//                 num1 = num1 >> 1;
+//                 k++;
+//             }
+//              System.out.println(k);
+//             ans += Math.pow(2, k);
+//             bits--;
+//             num1 = num1 >> 1;
+//             k++;
+//         }
+
+//         while (bits != 0) {
+//          System.out.println(k);
+//             ans += Math.pow(2, k);
+//             bits--;
+//             k++;
+//         }
+//         return ans;
