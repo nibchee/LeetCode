436e650e929ee080d5a635a2deafb3c529ee44c6
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-17 04:13:30 +0530

    LC-friends-of-appropriate-ages [Runtime: N/A; Memory: N/A]

--- binary-search/friends-of-appropriate-ages.java
+++ binary-search/friends-of-appropriate-ages.java
@@ -4,29 +4,44 @@ class Solution {
         int n = ages.length;
         int ans = 0;
         for (int i = n - 1; i >= 0; i--) {
-
-            int idx = bs(ages, i);
-            // System.out.println(idx);
-            if (idx != i) {
-                ans += (i - idx);
-                if (ages[i] == ages[idx])
-                    ans += (i - idx);
+            int idx1 = bs(ages, i, ages[i] * 0.5 + 7);
+            int idx2 = bs2(ages, i, ages[i]);
+            ans += (i - idx2);
+         //    System.out.println(idx1+" "+idx2);
+            if (idx1 != i) {
+                ans += (i - idx1);
             }
 
         }
         return ans;
     }
 
-    private int bs(int[] ages, int high) {
+    private int bs(int[] ages, int high, double target) {
         // System.out.println("IDX:"+high);
-        int x = ages[high];
-        double target = x * 0.5 + 7;
         int h = high - 1;
         int l = 0;
         int ans = high;
         while (l <= h) {
             int mid = l + (h - l) / 2;
             if (ages[mid] > target) {
+                // System.out.println(ans);
+                ans = mid;
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        // System.out.println("----------------------------");
+        return ans;
+    }
+private int bs2(int[] ages, int high, double target) {
+        // System.out.println("IDX:"+high);
+        int h = high - 1;
+        int l = 0;
+        int ans = high;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (ages[mid] >= target) {
                 // System.out.println(ans);
                 ans = mid;
                 h = mid - 1;
