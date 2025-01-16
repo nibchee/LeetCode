635f019273fb290ddd58d9d4ab8ef25d4a1fb3c5
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-17 04:47:46 +0530

    LC-friends-of-appropriate-ages [Runtime: 59 ms; Memory: 48.1 MB]

--- array/friends-of-appropriate-ages.java
+++ array/friends-of-appropriate-ages.java
@@ -1,17 +1,23 @@
 class Solution {
     public int numFriendRequests(int[] ages) {
         Arrays.sort(ages);
+        //for(int num:ages)
+        // System.out.print(num+" ");
+        //  System.out.println();
         int n = ages.length;
         int ans = 0;
         for (int i = n - 1; i >= 0; i--) {
-            int idx1 = bs(ages, i, ages[i] * 0.5 + 7);
-            int idx2 = bs2(ages, i, ages[i]);
+            double tar=ages[i] * 0.5 + 7;
+            int idx1 = bs(ages, i, tar);
+            int idx2 =(ages[i]>tar)?bs2(ages, i, ages[i]):i;
             ans += (i - idx2);
-         //    System.out.println(idx1+" "+idx2);
-            if (idx1 != i) {
-                ans += (i - idx1);
-            }
-
+            ans += (i - idx1);
+        // System.out.println(i+" " +ages[i]);
+        // System.out.println((ages[i]*0.5+7)+"--"+idx1+"-->"+ages[idx1]);
+        // System.out.println(ages[i]+"--"+idx2+"-->"+ages[idx2]);
+        //  System.out.println("Ans"+ans);
+        //   System.out.println("--------------------------");
+        
         }
         return ans;
     }
