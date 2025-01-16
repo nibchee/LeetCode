0da2ccf2e068b31f02a6edb875bae3ad4eb08a66
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-17 00:42:26 +0530

    LC-k-diff-pairs-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ binary-search/k-diff-pairs-in-an-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        HashMap<Integer,HashSet<Integer>> pairs=new HashMap<>();
+
+        for(int num:nums){
+           int num_plusK=num+k;
+           int num_minusK=num-k;
+          HashSet<Integer> s=pairs.get(nums);
+               
+           if(pairs.containsKey(num_plusK)){
+               if(s==null){
+                s=new HashSet<>();
+               }
+              s.add(num_plusK);
+               pairs.put(num,s);
+           }
+
+           if(pairs.containsKey(num_minusK)){
+               if(s==null){
+                s=new HashSet<>();
+               }
+              s.add(num_minusK);
+               pairs.put(num,s);
+           }
+
+           if(s==null)
+           pairs.put(num,null);
+
+        }
+       int ans=0;
+        for(Map.Entry<Integer,HashSet<Integer>> entry: pairs.entrySet()){
+           HashSet<Integer> set=entry.getValue();
+           if(set!=null){
+            ans+=set.size();
+           }
+        }
+        return ans;
+    }
+}
