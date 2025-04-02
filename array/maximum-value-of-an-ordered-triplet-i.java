15935977c5f1dc7417f9b78a60a57936cec00723
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-04-02 06:11:48 +0530

    LC-maximum-value-of-an-ordered-triplet-i [Runtime: 1 ms; Memory: 42.1 MB]

---
+++ array/maximum-value-of-an-ordered-triplet-i.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long res = 0;
+        for (int k = 2; k < n; k++) {
+            int maxPrefix = nums[0];
+            for (int j = 1; j < k; j++) {
+                res = Math.max(res, (long) (maxPrefix - nums[j]) * nums[k]);
+                maxPrefix = Math.max(maxPrefix, nums[j]);
+            }
+        }
+        return res;
+    }
+}
