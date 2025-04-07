160317b301198d3e25f0770e7a08504ccb93ecf8
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-04-08 02:12:08 +0530

    LC-min-cost-climbing-stairs [Runtime: N/A; Memory: N/A]

--- dynamic-programming/min-cost-climbing-stairs.java
+++ dynamic-programming/min-cost-climbing-stairs.java
@@ -1,10 +1,16 @@
 class Solution {
+     int dp[];
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
-        return helper(n,cost);
+        dp=new int[n+1];
+        helper(n,cost);
+        return dp[n];
     }
     private int helper(int n,int[] cost){
-        if(n<=1) return 0;
-     return Math.min(cost[n-1]+helper(n-1,cost),cost[n-2]+helper(n-2,cost));
+      if(n<=1) return 0;
+      int c1=cost[n-1]+(dp[n-1]!=0?dp[n-1]:helper(n-1,cost));
+      int c2=cost[n-2]+(dp[n-2]!=0?dp[n-2]:helper(n-2,cost));
+      dp[n]=Math.min(c1,c2);
+      return dp[n];
     }
 }
