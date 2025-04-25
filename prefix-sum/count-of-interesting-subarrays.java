8c4ede7093b0a8e77254f68ce6ce81aa3eabe1ab
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-04-26 03:38:57 +0530

    LC-count-of-interesting-subarrays [Runtime: 34 ms; Memory: 60.9 MB]

--- prefix-sum/count-of-interesting-subarrays.java
+++ prefix-sum/count-of-interesting-subarrays.java
@@ -1,32 +1,20 @@
 class Solution {
-    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
-      for(int i=0;i<nums.size();i++){
-        if(nums.get(i)%modulo==k){
-            nums.set(i,1);
-        }else{
-            nums.set(i,0);
+
+    public long countInterestingSubarrays(
+        List<Integer> nums,
+        int modulo,
+        int k
+    ) {
+        int n = nums.size();
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        long res = 0;
+        int prefix = 0;
+        cnt.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            prefix += nums.get(i) % modulo == k ? 1 : 0;
+            res += cnt.getOrDefault((prefix - k + modulo) % modulo, 0);
+            cnt.put(prefix % modulo, cnt.getOrDefault(prefix % modulo, 0) + 1);
         }
-      }
-
-      int l=0;
-      int cnt=0;
-      long ans=0;
-      int n=nums.size();
-      int p[]=new int[n];
-      int s[]=new int[n];
-      p[0]=nums.get(0);
-      s[n-1]=nums.get(n-1);
-
-      for(int i=0;i<n;i++){
-        if(i!=0 && nums.get(i)==1)
-          p[i]=nums[i]+p[i-1];
-          if(i!=0 && nums.get(n-i-1)==1)
-          s[i]=nums[i]+s[i+1];
-       }
-
-       for(int i=0;i<n;i++){
-         ans+=(s[i-1]-p[i])%k;
-       }  
-   return ans;
+        return res;
     }
 }
