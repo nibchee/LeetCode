ec648d0345ab4b685b17974469466c066ee2aaee
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-05-05 23:17:11 +0530

    LC-domino-and-tromino-tiling [Runtime: 0 ms; Memory: 40.4 MB]

--- dynamic-programming/domino-and-tromino-tiling.java
+++ dynamic-programming/domino-and-tromino-tiling.java
@@ -1,20 +1,14 @@
 class Solution {
-    final long mod = 1000000007;
-
+    private static final int mod = 1_000_000_007;
     public int numTilings(int n) {
-        return (int)dominoes(0, n, false);
-    }
-
-    private long dominoes(int i, int n, boolean possible) {
-        if (i == n) return possible ? 0 : 1;
-        if (i > n) return 0;
-
-        if (possible) {
-            return (dominoes(i + 1, n, false) + dominoes(i + 1, n, true)) % mod;
+        if (n <= 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 5;
+        int[] dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 5;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (int)((2L * dp[i-1] + dp[i-3]) % mod);
         }
-
-        return (dominoes(i + 1, n, false)
-              + dominoes(i + 2, n, false)
-              + 2 * dominoes(i + 2, n, true)) % mod;
+        return dp[n];
     }
 }
