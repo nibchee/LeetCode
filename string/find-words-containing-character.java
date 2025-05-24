5cfb81e44e8273895c95c835558c2e97d583ee11
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-05-25 02:06:26 +0530

    LC-find-words-containing-character [Runtime: 1 ms; Memory: 45 MB]

--- string/find-words-containing-character.java
+++ string/find-words-containing-character.java
@@ -1,10 +1,13 @@
 class Solution {
+
     public List<Integer> findWordsContaining(String[] words, char x) {
-        List<Integer> indexes=new ArrayList<>();
-       for(int i=0;i<words.length;i++){
-        if(words[i].indexOf(x)>=0)
-        indexes.add(i);
-       }
-    return indexes;
+        List<Integer> res = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            if (words[i].indexOf(x) != -1) {
+                res.add(i);
+            }
+        }
+        return res;
     }
 }
