6836d3376ceffb9413563e61fd0baafd81da43c4
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-08-06 00:49:34 +0530

    LC-number-of-integers-with-popcount-depth-equal-to-k-i [Runtime: N/A; Memory: N/A]

--- dynamic-programming/number-of-integers-with-popcount-depth-equal-to-k-i.java
+++ dynamic-programming/number-of-integers-with-popcount-depth-equal-to-k-i.java
@@ -1,40 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    long[] cd;
+    Map<Integer, Long> cd = new HashMap<>();
 
     private int findSetBit(int n) {
         int numberOfSetBit = 0;
         while (n > 0) {
-            n = n & (n - 1);
+            n &= (n - 1);
             numberOfSetBit++;
         }
         return numberOfSetBit;
     }
 
     private long computeDepth(int num) {
-        if(num==1 || num==0) return 0;
-        if (cd[num] != 0)
-            return cd[num];
+        if (num == 0 || num == 1)
+            return 0;
+
+        if (cd.containsKey(num))
+            return cd.get(num);
 
         int setBits = findSetBit(num);
-        cd[num] = computeDepth(setBits) + 1;
-        return cd[num];
+        long depth = computeDepth(setBits) + 1;
+        cd.put(num, depth);
+        return depth;
     }
 
     public long popcountDepth(long n, int k) {
-     if(k==0) return 1;
-    int max = (int) Math.min(n + 2, Integer.MAX_VALUE);
-    cd = new long[max];
-    cd[0] = cd[1] = 0;
+        if (k == 0)
+            return 1;
 
-    long ans = 0;
-    for (int i = 2; i <= n; i++) {
-        cd[i] = computeDepth(i);
-        //System.out.println(i+" N "+cd[i]);
-        if (cd[i] == k)
-            ans++;
+        long ans = 0;
+        for (int i = 2; i <= n; i++) {
+            long depth = computeDepth(i);
+            if (depth == k)
+                ans++;
+        }
+        return ans;
     }
-
-    return ans;
-}
-
 }
