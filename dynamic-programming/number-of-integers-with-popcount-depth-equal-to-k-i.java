e8ac859c430de16a545af9f8904616bee84b216d
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-08-06 00:41:50 +0530

    LC-number-of-integers-with-popcount-depth-equal-to-k-i [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/number-of-integers-with-popcount-depth-equal-to-k-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    long[] cd;
+
+    private int findSetBit(int n) {
+        int numberOfSetBit = 0;
+        while (n > 0) {
+            n = n & (n - 1);
+            numberOfSetBit++;
+        }
+        return numberOfSetBit;
+    }
+
+    private long computeDepth(int num) {
+        if(num==1 || num==0) return 0;
+        if (cd[num] != 0)
+            return cd[num];
+
+        int setBits = findSetBit(num);
+        cd[num] = computeDepth(setBits) + 1;
+        return cd[num];
+    }
+
+    public long popcountDepth(long n, int k) {
+    if (k == 1)
+        return 1;
+
+    int max = (int) Math.min(n + 2, Integer.MAX_VALUE);
+    cd = new long[max];
+    cd[0] = cd[1] = 0;
+
+    long ans = 0;
+    for (int i = 2; i <= n; i++) {
+        cd[i] = computeDepth(i);
+        if (cd[i] == k)
+            ans++;
+    }
+
+    return ans;
+}
+
+}
