a750237d73480bb6c78bb381b1e7fdbf29d50091
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-08-06 00:48:24 +0530

    LC-number-of-integers-with-popcount-depth-equal-to-k-i [Runtime: N/A; Memory: N/A]

--- combinatorics/number-of-integers-with-popcount-depth-equal-to-k-i.java
+++ combinatorics/number-of-integers-with-popcount-depth-equal-to-k-i.java
@@ -21,7 +21,7 @@ class Solution {
     }
 
     public long popcountDepth(long n, int k) {
-     if(n==1 && k==0) return 1;
+     if(k==0) return 1;
     int max = (int) Math.min(n + 2, Integer.MAX_VALUE);
     cd = new long[max];
     cd[0] = cd[1] = 0;
