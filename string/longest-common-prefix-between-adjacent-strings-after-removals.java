91328ab03ce91f0842fad52359828fff26f8b981
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-08-13 00:15:15 +0530

    LC-longest-common-prefix-between-adjacent-strings-after-removals [Runtime: N/A; Memory: N/A]

--- string/longest-common-prefix-between-adjacent-strings-after-removals.java
+++ string/longest-common-prefix-between-adjacent-strings-after-removals.java
@@ -5,15 +5,15 @@ class Solution {
         while (i < minLength && s1.charAt(i) == s2.charAt(i)) {
             i++;
         }
-        return i; 
+        return i;
     }
 
     public int[] longestCommonPrefix(String[] words) {
         int n = words.length;
         int[][] longPrefix = new int[n - 1][2];
-        
+
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        
+
         for (int i = 0; i < n - 1; i++) {
             longPrefix[i][0] = getPrefixLength(words[i], words[i + 1]);
             if (i + 2 < n) {
@@ -22,28 +22,28 @@ class Solution {
                 longPrefix[i][1] = 0;
             }
             maxHeap.add(longPrefix[i][0]);
-           // maxHeap.add(longPrefix[i][1]);
+            // maxHeap.add(longPrefix[i][1]);
         }
 
         int[] ans = new int[n];
         for (int i = 0; i < longPrefix.length; i++) {
-            System.out.println(longPrefix[i][0]+" "+longPrefix[i][1]);
-            if(i-1>=0){
-            maxHeap.remove(longPrefix[i-1][0]);
-            maxHeap.add(longPrefix[i-1][1]);
+            //System.out.println(longPrefix[i][0] + " " + longPrefix[i][1]);
+            if (i - 1 >= 0) {
+                maxHeap.remove(longPrefix[i - 1][0]);
+                maxHeap.add(longPrefix[i - 1][1]);
             }
             maxHeap.remove(longPrefix[i][0]);
-            
+
             ans[i] = maxHeap.peek() != null ? maxHeap.peek() : 0;
-              if(i-1>=0){
-            maxHeap.add(longPrefix[i-1][0]);
-            maxHeap.remove(longPrefix[i-1][1]);
+            if (i - 1 >= 0) {
+                maxHeap.add(longPrefix[i - 1][0]);
+                maxHeap.remove(longPrefix[i - 1][1]);
             }
             maxHeap.add(longPrefix[i][0]);
-          
-          
+
         }
-        ans[n-1]=maxHeap.peek() != null ? maxHeap.peek() : 0;
+        maxHeap.remove(longPrefix[n-2][0]);
+        ans[n - 1] = maxHeap.peek() != null ? maxHeap.peek() : 0;
         return ans;
     }
 
