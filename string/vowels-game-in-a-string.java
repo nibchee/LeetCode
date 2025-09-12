43897bb1d4b09e62a9f3ddd2b69845db1ee87b30
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-09-12 22:59:14 +0530

    LC-vowels-game-in-a-string [Runtime: 7 ms; Memory: 45.5 MB]

--- string/vowels-game-in-a-string.java
+++ string/vowels-game-in-a-string.java
@@ -5,6 +5,7 @@ class Solution {
             if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
             cnt++;
         }
-        return (cnt!=0 && (cnt%2==0 || (cnt%2==1 && s.length()>cnt)));
+        //return (cnt!=0 && (cnt%2==0 || (cnt%2==1 && s.length()>cnt)));
+        return (cnt%2==1 || (cnt!=0 && cnt%2==0));
     }
 }
