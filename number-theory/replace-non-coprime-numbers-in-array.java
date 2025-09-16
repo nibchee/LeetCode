2389fc1670dc1299f21561783a66b9d81a29787a
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-09-16 23:45:28 +0530

    LC-replace-non-coprime-numbers-in-array [Runtime: 34 ms; Memory: 63.1 MB]

---
+++ number-theory/replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        List<Integer> stack = new ArrayList<>();
+        
+        for (int num : nums) {
+            stack.add(num);
+            while (stack.size() > 1) {
+                int a = stack.get(stack.size() - 1);
+                int b = stack.get(stack.size() - 2);
+                int g = gcd(a, b);
+                if (g > 1) { // non-coprime
+                    stack.remove(stack.size() - 1);
+                    stack.remove(stack.size() - 1);
+                    long lcm = (long) a / g * b; 
+                    stack.add((int) lcm);
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        return stack;
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
