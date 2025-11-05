8bccef578b6acb97683d3aee572b181003731de7
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-11-05 22:34:39 +0530

    LC-find-x-sum-of-all-k-long-subarrays-ii [Runtime: 468 ms; Memory: 203.8 MB]

--- heap-priority-queue/find-x-sum-of-all-k-long-subarrays-ii.java
+++ heap-priority-queue/find-x-sum-of-all-k-long-subarrays-ii.java
@@ -1,53 +1,117 @@
+class Helper {
+
+    private int x;
+    private long result;
+    private TreeSet<Pair> large, small;
+    private Map<Integer, Integer> occ;
+
+    private static class Pair implements Comparable<Pair> {
+
+        int first;
+        int second;
+
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public int compareTo(Pair other) {
+            if (this.first != other.first) {
+                return Integer.compare(this.first, other.first);
+            }
+            return Integer.compare(this.second, other.second);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Pair pair = (Pair) obj;
+            return first == pair.first && second == pair.second;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(first, second);
+        }
+    }
+
+    public Helper(int x) {
+        this.x = x;
+        this.result = 0;
+        this.large = new TreeSet<>();
+        this.small = new TreeSet<>();
+        this.occ = new HashMap<>();
+    }
+
+    public void insert(int num) {
+        if (occ.containsKey(num) && occ.get(num) > 0) {
+            internalRemove(new Pair(occ.get(num), num));
+        }
+        occ.put(num, occ.getOrDefault(num, 0) + 1);
+        internalInsert(new Pair(occ.get(num), num));
+    }
+
+    public void remove(int num) {
+        internalRemove(new Pair(occ.get(num), num));
+        occ.put(num, occ.get(num) - 1);
+        if (occ.get(num) > 0) {
+            internalInsert(new Pair(occ.get(num), num));
+        }
+    }
+
+    public long get() {
+        return result;
+    }
+
+    private void internalInsert(Pair p) {
+        if (large.size() < x || p.compareTo(large.first()) > 0) {
+            result += (long) p.first * p.second;
+            large.add(p);
+            if (large.size() > x) {
+                Pair toRemove = large.first();
+                result -= (long) toRemove.first * toRemove.second;
+                large.remove(toRemove);
+                small.add(toRemove);
+            }
+        } else {
+            small.add(p);
+        }
+    }
+
+    private void internalRemove(Pair p) {
+        if (p.compareTo(large.first()) >= 0) {
+            result -= (long) p.first * p.second;
+            large.remove(p);
+            if (!small.isEmpty()) {
+                Pair toAdd = small.last();
+                result += (long) toAdd.first * toAdd.second;
+                small.remove(toAdd);
+                large.add(toAdd);
+            }
+        } else {
+            small.remove(p);
+        }
+    }
+}
+
 class Solution {
-  public long[] findXSum(int[] nums, int k, int x) {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    int n = nums.length;
-    PriorityQueue<Integer> pq = new PriorityQueue<>(x,
-        (a, b) -> {
-          if (map.get(b) == map.get(a))
-            return b - a;
-          return map.get(b) - map.get(a);
-        });
 
-    for (int i = 0; i < k; i++) {
-      map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    public long[] findXSum(int[] nums, int k, int x) {
+        Helper helper = new Helper(x);
+        List<Long> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            helper.insert(nums[i]);
+            if (i >= k) {
+                helper.remove(nums[i - k]);
+            }
+            if (i >= k - 1) {
+                ans.add(helper.get());
+            }
+        }
+
+        return ans.stream().mapToLong(Long::longValue).toArray();
     }
-
-    for (int i = 0; i < k; i++) {
-      if (!pq.contains(nums[i]))
-        pq.add(nums[i]);
-    }
-
-    int j = k - 1, i = 0;
-    long ans[] = new long[n - k + 1];
-
-    while (j < nums.length) {
-      long sum = 0l;
-      for (int y = 0; y < x && !pq.isEmpty(); y++) {
-        int e = pq.remove();
-       // System.out.println("ele " + e + " " + map.get(e));
-        sum += (long)e * (long)map.get(e);
-      }
-      while (!pq.isEmpty()) {
-        pq.remove();
-      }
-      //System.out.println("Sum " + sum);
-      map.put(nums[i], map.get(nums[i]) - 1);
-      if (map.get(nums[i]) == 0)
-        map.remove(nums[i]);
-      ans[i] = sum;
-      i++;
-      j++;
-      if (j < n) {
-        map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-      }
-      for (int z = i; z <= j && z < n; z++) {
-        if (!pq.contains(nums[z]))
-          pq.add(nums[z]);
-      }
-    }
-
-    return ans;
-  }
-
 }
