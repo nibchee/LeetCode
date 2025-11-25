cc3599dbb7f937aaf8b09bc43eb57c6df03022c7
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-11-25 19:22:31 +0530

    LC-smallest-integer-divisible-by-k [Runtime: 2 ms; Memory: 42 MB]

--- math/smallest-integer-divisible-by-k.java
+++ math/smallest-integer-divisible-by-k.java
@@ -1,14 +1,12 @@
 class Solution {
-    public int smallestRepunitDivByK(int k) {
-     int modk=k%10;
-     if(modk==2||modk==4||modk==5||modk==6||modk==8 ||modk%10==0)
-     return -1;
-     int ans=1;
-     int len=1;
-     while(ans%k!=0){
-      ans=ans*10+1;
-      len++;
-     }
-     return len;   
+    public int smallestRepunitDivByK(int K) {
+        int remainder = 0;
+        for (int length_N = 1; length_N <= K; length_N++) {
+            remainder = (remainder * 10 + 1) % K;
+            if (remainder == 0) {
+                return length_N;
+            }
+        }
+        return -1;
     }
 }
