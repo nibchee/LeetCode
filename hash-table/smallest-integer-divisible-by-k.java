6f92d3cc26684b2697263fc297a7911ae522d07c
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-11-25 19:15:37 +0530

    LC-smallest-integer-divisible-by-k [Runtime: N/A; Memory: N/A]

--- hash-table/smallest-integer-divisible-by-k.java
+++ hash-table/smallest-integer-divisible-by-k.java
@@ -3,6 +3,12 @@ class Solution {
      int modk=k%10;
      if(modk==2||modk==4||modk==5||modk==6||modk==8 ||modk%10==0)
      return -1;
-     return 1;   
+     int ans=1;
+     int len=1;
+     while(ans%k!=0){
+      ans=ans*10+1;
+      len++;
+     }
+     return len;   
     }
 }
